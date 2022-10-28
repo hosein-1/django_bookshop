6ebab01807b7448b6427fdf134f99b264c19cc47
hosein-1 <hoseinlaki16@gmail.com>
2022-10-28 13:26:41 +0330

    remove HomePageView and the urls relate to it

--- pages/urls.py
+++ pages/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomePageView.as_view(), name='home'),
     path('about/', views.AboutUsPageView.as_view(), name='about'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,5 @@
 from django.views import generic
 
 
-class HomePageView(generic.TemplateView):
-    template_name = 'pages/home.html'
-
-
 class AboutUsPageView(generic.TemplateView):
     template_name = 'pages/about_us.html'

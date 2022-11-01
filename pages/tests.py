729549867b46329b59c803eb2467ab63331d729b
hosein-1 <hoseinlaki16@gmail.com>
2022-11-01 22:39:05 +0330

    change home tests to contact_us tests

--- pages/tests.py
+++ pages/tests.py
@@ -2,18 +2,18 @@ from django.test import TestCase
 from django.urls import reverse
 
 
-class HomePageTests(TestCase):
-    def test_home_page_url(self):
+class ContactUsPageTests(TestCase):
+    def test_contact_us_page_url(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
-    def test_home_page_url_by_name(self):
-        response = self.client.get(reverse('home'))
+    def test_contact_us_page_url_by_name(self):
+        response = self.client.get(reverse('contact_us'))
         self.assertEqual(response.status_code, 200)
 
-    def test_home_page_template_used(self):
-        response = self.client.get(reverse('home'))
-        self.assertTemplateUsed(response, 'pages/home.html')
+    def test_contact_us_page_template_used(self):
+        response = self.client.get(reverse('contact_us'))
+        self.assertTemplateUsed(response, 'pages/contact_us.html')
 
 
 class AboutUsPageTests(TestCase):

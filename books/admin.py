c9de812d17029a6ea9257cd1c283127f858a72d7
hosein-1 <hoseinlaki16@gmail.com>
2022-11-03 15:13:09 +0330

    add CategoryAdmin

--- books/admin.py
+++ books/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Book, Category
 
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'price', )
     prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+    prepopulated_fields = {'slug': ('name',)}

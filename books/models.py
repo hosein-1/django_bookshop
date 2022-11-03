76c63f34de25924c04190ac645ccd4acfa990137
hosein-1 <hoseinlaki16@gmail.com>
2022-11-03 13:14:15 +0330

    add Category model and migrations files

--- books/models.py
+++ books/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=100, unique=True)
+
+
 class Book(models.Model):
     title = models.CharField(max_length=250)
     author = models.CharField(max_length=250)
@@ -12,3 +17,4 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+

15d69eaddd675dde5eac2b2c403c32de0bdfe432
hosein-1 <hoseinlaki16@gmail.com>
2022-11-08 23:08:29 +0330

    rename books_list.html to book_list.html

--- books/views.py
+++ books/views.py
@@ -5,5 +5,6 @@ from .models import Book
 
 class BookListView(generic.ListView):
     model = Book
-    template_name = 'books/books_list.html'
+    template_name = 'books/book_list.html'
     context_object_name = 'books'
+

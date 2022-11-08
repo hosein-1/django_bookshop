4c6ad344906fa3ada99dd9b12103c607aa2e8a4e
hosein-1 <hoseinlaki16@gmail.com>
2022-11-08 23:10:48 +0330

    add BookDetailView with url

--- books/urls.py
+++ books/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
+
 urlpatterns = [
     path('', views.BookListView.as_view(), name='book_list'),
+    re_path(r'book/(?P<pk>[0-9]+)/(?P<slug>[-\w]+)/$', views.BookDetailView.as_view(), name='book_detail'),
 ]
--- books/views.py
+++ books/views.py
@@ -8,3 +8,9 @@ class BookListView(generic.ListView):
     template_name = 'books/book_list.html'
     context_object_name = 'books'
 
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    slug_field = 'slug'
+    template_name = 'books/book_detail.html'
+    context_object_name = 'book'

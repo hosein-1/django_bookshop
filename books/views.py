1b6204af190ea0950322970cf2093d31bf629888
hosein-1 <hoseinlaki16@gmail.com>
2022-11-13 21:44:14 +0330

    add comment_form as context

--- books/views.py
+++ books/views.py
@@ -1,6 +1,7 @@
 from django.views import generic
 
 from .models import Book
+from .forms import CommentForm
 
 
 class BookListView(generic.ListView):
@@ -14,3 +15,8 @@ class BookDetailView(generic.DetailView):
     slug_field = 'slug'
     template_name = 'books/book_detail.html'
     context_object_name = 'book'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comment_form'] = CommentForm()
+        return context

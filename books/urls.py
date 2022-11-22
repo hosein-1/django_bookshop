fc54025bc2da739ade5a977f89f33d4ac353cb74
hosein-1 <hoseinlaki16@gmail.com>
2022-11-22 23:48:28 +0330

    remove CommentCreateView url and update book_detail_view

--- books/urls.py
+++ books/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.BookListView.as_view(), name='book_list'),
-    re_path(r'book/(?P<pk>[0-9]+)/(?P<slug>[-\w]+)/$', views.BookDetailView.as_view(), name='book_detail'),
-    path('comment/<int:pk>/', views.CommentCreateView.as_view(), name='comment_create'),
+    re_path(r'book/(?P<pk>[0-9]+)/(?P<slug>[-\w]+)/$', views.book_detail_view, name='book_detail'),
+
 ]

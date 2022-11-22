90ba4a19517a69a321feee4cfeab259f9ed69555
hosein-1 <hoseinlaki16@gmail.com>
2022-11-22 23:49:36 +0330

    update BookDetailView to functional view

--- books/views.py
+++ books/views.py
@@ -1,4 +1,5 @@
 from django.views import generic
+from django.shortcuts import get_object_or_404, render
 
 from .models import Book
 from .forms import CommentForm
@@ -10,13 +11,17 @@ class BookListView(generic.ListView):
     context_object_name = 'books'
 
 
-class BookDetailView(generic.DetailView):
-    model = Book
-    slug_field = 'slug'
-    template_name = 'books/book_detail.html'
-    context_object_name = 'book'
+def book_detail_view(request, pk, slug):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            book_object = form.save(commit=False)
+            book_object.author = request.user
+            book_object.book = book
+            form.save()
+        return render(request, 'books/book_detail.html', {'form': form, 'book': book})
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['comment_form'] = CommentForm()
-        return context
+    else:
+        form = CommentForm()
+        return render(request, 'books/book_detail.html', {'form': form, 'book': book})

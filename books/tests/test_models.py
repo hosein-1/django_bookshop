098b8784759fd07131f77ec135089e0b3a07ad4b
hosein-1 <hoseinlaki16@gmail.com>
2023-01-25 18:36:50 +0330

    add tests for CommentModel

--- books/tests/test_models.py
+++ books/tests/test_models.py
@@ -47,3 +47,54 @@ class BookModelTest(TestCase):
         book = Book.objects.get(id=1)
         self.assertEqual(book.get_absolute_url(), '/book/1/test-title-1/')
 
+
+class CommentModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.category1 = Category.objects.create(name='novel', slug='novel')
+        cls.book2 = Book.objects.create(
+            title='test title2',
+            author='ali',
+            description='This is a test',
+            price=4000,
+            publisher='test_publisher',
+            translator='saeed',
+            cover=SimpleUploadedFile('file.jpg', b"file_content", content_type='image/jpeg'),
+            slug='test-title-2',
+
+        )
+        cls.book2.category.set([cls.category1.pk])
+
+        cls.user = get_user_model().objects.create(email='hosein@gmail.com')
+        cls.comment1 = Comment.objects.create(
+            text='This is a comment for test.',
+            author=cls.user,
+            book=cls.book2,
+            datetime_created=datetime.datetime.now(),
+        )
+        cls.comment2 = Comment.objects.create(
+            text='This is a comment for test1.',
+            author=cls.user,
+            book=cls.book2,
+            datetime_created=datetime.datetime.now(),
+            active=False,
+            parent=cls.comment1,
+
+        )
+
+    def test_comment_text(self):
+        self.assertEqual(self.comment1.text, 'This is a comment for test.')
+
+    def test_comment_is_parent_or_not(self):
+        self.assertEqual(self.comment1.parent, None)
+        self.assertEqual(self.comment2.parent, self.comment1)
+
+    def test_active_comment_show_in_book_detail(self):
+        response = self.client.get(reverse('book_detail', kwargs={'pk': self.book2.pk, 'slug': self.book2.slug}))
+        self.assertEqual(self.comment1.active, True)
+        self.assertContains(response, self.comment1.text)
+
+    def test_inactive_comment_not_show_in_book_detail(self):
+        response = self.client.get(reverse('book_detail', kwargs={'pk': self.book2.pk, 'slug': self.book2.slug}))
+        self.assertEqual(self.comment2.active, False)
+        self.assertNotContains(response, self.comment2.text)

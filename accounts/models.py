41ec222afe924ab26a7375fb3abc0d696fd19894
hosein-1 <hoseinlaki16@gmail.com>
2023-02-04 14:53:58 +0330

    add avatar and bio fields to CustomUser model

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+
+from random import choice
+
+
+def set_default_avatar():
+    # Returns a random avatar image.
+    avatars = (
+        'avatar1.jpg',
+        'avatar2.jpg',
+    )
+    return choice(avatars)
 
 
 class CustomUser(AbstractUser):
-    pass
+    avatar = models.ImageField(
+        _('Avatar'),
+        upload_to='accounts/avatars/',
+        default=f'accounts/default_avatars/{set_default_avatar()}')
+
+    bio = models.TextField(_('Biography'), blank=True)
+
+

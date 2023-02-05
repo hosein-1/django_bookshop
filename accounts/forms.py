9a602516ffb09a554340b49d145134731cb80f34
hosein-1 <hoseinlaki16@gmail.com>
2023-02-05 16:28:50 +0330

    add ProfileForm

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django import forms
+
 from .models import AbstractUser
+from .models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -12,3 +15,14 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = AbstractUser
         fields = ('username', 'email', )
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = ('avatar', 'username', 'email', 'bio', 'first_name', 'last_name', )
+
+    def __init__(self, *args, **kwargs):
+        super(ProfileForm, self).__init__(*args, **kwargs)
+        self.fields['email'].disabled = True
+        self.fields['username'].disabled = True

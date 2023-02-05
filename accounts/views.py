1d816e95991815f6c81dcc3eb2400f63c3c2a748
hosein-1 <hoseinlaki16@gmail.com>
2023-02-05 16:29:56 +0330

    add profile view for edit profile or show the profile for user

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .forms import ProfileForm
+
+
+@login_required
+def edit_profile_view(request):
+
+    form = ProfileForm(request.POST or None, request.FILES, instance=request.user)
+
+    if form.is_valid():
+        form.save()
+
+    return render(request, 'accounts/profile.html', {'form': form})

06e38bd5dd99d12d0b2d2c15e70df611b0877ad1
hosein-1 <hoseinlaki16@gmail.com>
2023-02-06 17:30:51 +0330

    add context_processors for cart

--- config/settings.py
+++ config/settings.py
@@ -76,6 +76,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # Custom context processors
+                'cart.context_processors.cart',
             ],
         },
     },

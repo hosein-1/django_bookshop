4e694f5611092e0419caeb56d30db26200552064
hosein-1 <hoseinlaki16@gmail.com>
2023-02-06 19:16:58 +0330

    add cart_detail and add_to_cart for cart views

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
-# Create your views here.
+from .cart import Cart
+from books.models import Book
+from .forms import AddToCartBookForm
+
+
+def cart_detail_view(request):
+    cart = Cart(request)
+    return render(request, 'cart/cart_detail.html', {'cart': cart})
+
+
+def add_to_cart_view(request, book_id):
+    cart = Cart(request)
+    book = get_object_or_404(Book, id=book_id)
+    form = AddToCartBookForm(request.POST)
+
+    if form.is_valid():
+        cleaned_data = form.cleaned_data
+        quantity = cleaned_data['quantity']
+        cart.add(book, quantity)
+    return redirect('cart:cart_detail')

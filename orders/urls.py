f24aa48a3b168b9e44553a060fffe4c8ff14bfd7
hosein-1 <hoseinlaki16@gmail.com>
2023-02-13 19:57:59 +0330

    implement create_order view

--- orders/urls.py
+++ orders/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
+from .views import create_order
+
+app_name = 'orders'
 
 urlpatterns = [
     path('', create_order, name='order_create'),
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from cart.cart import Cart
+from .forms import OrderForm
+from .models import OrderItem
 
-# Create your views here.
+
+@login_required
+def create_order(request):
+    order_form = OrderForm()
+    cart = Cart(request)
+
+    if len(cart) == 0:
+        redirect('books:book_list')
+
+    if request.method == 'POST':
+        order_form = OrderForm(request.POST)
+        if order_form.is_valid():
+            order_object = order_form.save(commit=False)
+            order_object.user = request.user
+            order_object.save()
+
+            for item in cart:
+                book = item['book_object']
+                OrderItem.objects.create(
+                    order=order_object,
+                    book=book,
+                    quantity=item['quantity'],
+                    price=book.price
+                )
+
+            cart.clear()
+            request.session['order_id'] = order_object.id
+            return redirect('payment:payment_process')
+
+    return render(request, 'orders/order_create.html', {'order_form': order_form})
